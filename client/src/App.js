aed585fcd3d147fdda6f83e087eb4e310c55776d
sachinzzzzz <sachinmunda954@gmail.com>
2024-11-14 13:11:48 +0530

    adjust redirecting

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import React from 'react';
 // import { getData } from './services/api';
 import Home from './components/Home/home'
 import Login from './components/accounts/login'
+import Loading from './components/Loading/loading'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
     <div className="container mt-5">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Loading />}></Route>
         <Route path='/login' element={<Login/>}></Route>
       </Routes>
       </BrowserRouter>
